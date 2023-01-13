d22d3c23c3614305647717e4b904ff3ddbafd56a
KritsadaRachawong <122585177+KritsadaRachawong@users.noreply.github.com>
2023-01-13 13:45:05 +0700

    intital

---
+++ deployweb2azure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace deployweb2azure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
